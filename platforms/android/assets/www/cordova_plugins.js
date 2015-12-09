56d7e26c0eac6e187bfdc1667bfc2290f5ef99c7
kalyan <kalyan@staging.eimpressive.com>
2015-12-09 18:40:21 +0530

    myshop1

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.paypal.cordova.mobilesdk/www/cdv-plugin-paypal-mobile-sdk.js",
+        "id": "com.paypal.cordova.mobilesdk.PayPalMobile",
+        "clobbers": [
+            "PayPalMobile"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "com.paypal.cordova.mobilesdk": "3.1.12",
+    "org.apache.cordova.console": "0.2.13"
+}
+// BOTTOM OF METADATA
+});
